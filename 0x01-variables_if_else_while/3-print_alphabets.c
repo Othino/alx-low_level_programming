0558e5d356533cdae3b88f953de47bb9eecb21dd
Othino <micahothino44@gmail.com>
2022-07-01 06:20:29 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Use 'putchar' to print lowercase and then uppercase alphabet.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c), c++;
+}
+
+char c = 'A';
+while (c <= 'Z')
+{
+putchar(c), c++;
+}
+
+putchar('\n');
+
+return (0)
+}
