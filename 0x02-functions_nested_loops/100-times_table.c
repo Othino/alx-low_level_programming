0f583dcd9081b1174e6b3709d101c76f3743e42f
Othino <micahothino44@gmail.com>
2022-07-05 12:05:17 -0700

    #include "holberton.h"

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,57 +8,43 @@
 
 void print_times_table(int n)
 {
-int i, j;
-if (n > 0 && n < 15)
+int a, b, op;
+
+if (n >= 0 && n <= 15)
 {
 
-for (i = 0; i <= n; i++)
-
+for (a = 0; a <= n; a++)
 {
-_putchar('0');
-for (j = i; j <= n; j++)
-putformat(i * j);
-_putchar('\n');
-}
-}
-}
+_putchar(48);
 
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
- */
-
-void putformat(int n)
+for (b = 1; b <= n; b++)
 {
+op = a * b;
+_putchar(44);
+_putchar(32);
 
-if (n <= 9)
-
+if (op <= 9)
 {
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(n + '0');
+_putchar(32);
+_putchar(32);
+_putchar(op + 48);
 }
 
-else if (n > 9 && n <= 99)
-
+else if (op <= 99)
 {
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
+_putchar(32);
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
 }
 
 else
-
 {
-_putchar(',');
-_putchar(' ');
-_putchar(n / 100 + '0');
-_putchar(n / 10 % 10 + '0');
-_putchar(n % 10 + '0');
+_putchar(((op / 100) % 10) + 48);
+_putchar(((op / 10) % 10) + 48);
+_putchar((op % 10) + 48);
+}
+}
+_putchar('\n');
+}
 }
 }
