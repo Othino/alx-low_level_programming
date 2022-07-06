f0853741dbc678d6ba2ab937622d2645ec5c1291
Othino <micahothino44@gmail.com>
2022-07-06 03:32:41 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests function that prints if integer is + or -
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
