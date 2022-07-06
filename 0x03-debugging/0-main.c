bb66eaab2c09cb87744cc37154dac70d17a08267
Othino <micahothino44@gmail.com>
2022-07-06 05:04:13 -0700

    Leap year

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,15 @@
 
 /**
  * main - Tests function that prints if integer is positive or negative
+ *
  * Return: Always 0
  */
 
 int main(void)
 
 {
-int i;
 
+int i;
 i = 0;
 positive_or_negative(i);
 
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,28 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int positive_or_negative(void)
+* main - Print whether the number is positive or negative.
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n > 0)
 printf("%d is positive\n", n);
-
-else if (n == 0)
-printf("%d is zero\n", n);
-
-else
+if (n < 0)
 printf("%d is negative\n", n);
-
+if (n == 0)
+printf("%d is zero\n", n);
 return (0);
 }
