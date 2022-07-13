56336775934c9d9922935a2a0fdcd6e47046a0bc
Othino <micahothino44@gmail.com>
2022-07-13 08:38:19 -0700

    Corrections

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,18 +9,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int srclen = 0, i = 0;
-char *temp = dest, *start = src;
-while (*src)
+int srclen = 0,
+i = 0;
+
+while (src[i] != '\0' && i < n)
 {
-srclen++;
-src++;
+dest[i] = src[i];
+i++;
 }
-srclen++;
-if (n > srclen)
-n = srclen;
-src = start;
-for (; i < n; i++)
-*dest++ = *src++;
-return (temp);
+
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
