9d92fd1683b641ccd41e8d8620f2203d75e6c546
Othino <micahothino44@gmail.com>
2022-07-19 12:27:12 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,7 +6,7 @@
  * @c: iterator.
  * Return: 1 if n is prime number, 0 if n is not prime number.
  */
-int is_prime(unsigned int n, unsigned int c);
+int is_prime(unsigned int n, unsigned int c)
 {
 if (n % c == 0)
 {
