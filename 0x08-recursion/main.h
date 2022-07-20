f807ee548351af801248c83dc32117bf21cf1a71
Othino <micahothino44@gmail.com>
2022-07-20 00:37:48 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,6 @@ int is_palindrome(char *s);
 int get_length(char *s);
 int my_pal(char *s, int l);
 int wildcmp(char *s1, char *s2);
+int findsrc(char *s, char c, int i, int p);
 
 #endif /* MAIN.H */
