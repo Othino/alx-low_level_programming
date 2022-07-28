489495fd47374b0e616307f6839d49731ccc2340
Othino <micahothino44@gmail.com>
2022-07-28 03:23:54 -0700

    Fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,44 +2,63 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenate 2 strings, only n bytes of s2
- * @s1: string 1
- * @s2: string 2
- * @n: bytes to include of s2
- * Return: NULL if fail, else pointer to malloc memory
+ * len - messures string length
+ * @s: string to be messured
+ * Return: length of string
  */
+unsigned int len(char *s)
+{
+unsigned int i;
+for (i = 0; s[i] != '\0'; i++)
+;
+return (i);
+}
 
+/**
+ * string_nconcat - joins 2 strings up to n char of s2
+ * @s1: string to be coppied completly
+ * @s2: string to be concanated by n chars
+ * @n: number of chars to match
+ * Return: pointer to string
+ *
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *sout;
-unsigned int ls1, ls2, lsout, i;
+unsigned int i, i2;
+char *str;
 
 if (s1 == NULL)
+{
 s1 = "";
+}
 
 if (s2 == NULL)
+{
 s2 = "";
+}
 
-for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-;
-
-for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-;
-
-if (n > ls2)
-n = ls2;
-lsout = ls1 + n;
-sout = malloc(lsout + 1);
-
-if (sout == NULL)
-return (NULL);
-
-for (i = 0; i < lsout; i++)
-if (i < ls1)
-sout[i] = s1[i];
+if (n >= len(s2))
+str = malloc((len(s1) + len(s2) + 1) * sizeof(char));
 else
-sout[i] = s2[i - ls1];
-sout[i] = '\0';
+str = malloc((len(s1) + n + 1) * sizeof(char));
 
-return (sout);
+if (str == NULL)
+{
+return (NULL);
+}
+i = 0;
+i2 = 0;
+while (s1[i] != '\0')
+{
+str[i] = s1[i];
+i++;
+}
+while ((s2[i2] != '\0') && (i2 < n))
+{
+str[i] = s2[i2];
+i++;
+i2++;
+}
+str[i] = '\0';
+return (str);
 }
