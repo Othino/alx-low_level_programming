dd7960cd3cf9faef42760559fbdb6181d97b25f2
Othino <micahothino44@gmail.com>
2022-08-18 04:08:55 -0700

    11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,11 +8,14 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
+	unsigned long int p;
 
-	max <<= index;
-	if (max == 0)
+	if (index > 64)
 		return (-1);
-	*n |= max;
+
+	for (p = 1; index > 0; index--, p *= 2)
+		;
+	*n += p;
+
 	return (1);
 }
