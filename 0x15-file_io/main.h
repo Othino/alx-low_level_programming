9ba3f6a14d64d2f7bebfd1861c86b26a5b48cfef
Othino <micahothino44@gmail.com>
2022-08-22 08:48:25 -0700

    Update

--- 0x15-file_io/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MAIN.H_
-#define _MAIN.H_
-
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-
-#endif /* __HOLBERTON_H__ */
