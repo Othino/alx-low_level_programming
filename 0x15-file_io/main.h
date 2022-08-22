d96dd777386d7bed074e2891b14ccaadfa9b3b7d
Othino <micahothino44@gmail.com>
2022-08-22 10:28:24 -0700

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,14 @@
-#ifndef __MAIN.H__
-#define __MAIN.H__
+#ifndef _MAIN.H_
+#define _MAIN.H_
+
+int _putchar(char c);
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
