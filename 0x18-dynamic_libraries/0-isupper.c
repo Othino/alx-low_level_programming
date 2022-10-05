00df1b6c72863cc6fea4f7088bccc7673fadd5f1
Othino <micahothino44@gmail.com>
2022-10-05 03:33:29 -0700

    .c files

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for the uppercase characters
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
+ */
+
+int _isupper(int c)
+
+{
+
+if (c >= 65 && c <= 90)
+{
+
+return (1);
+}
+
+else
+{
+
+return (0);
+}
+}
