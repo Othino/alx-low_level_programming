0ae3454cf52123671ba64f34783162c6f0036f77
Othino <micahothino44@gmail.com>
2022-10-05 12:38:00 -0700

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Othino/alx-low_level_programming/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -qP /tmp/ https://github.com/Othino/alx-low_level_programming/raw/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD="/tmp/libmask.so"
